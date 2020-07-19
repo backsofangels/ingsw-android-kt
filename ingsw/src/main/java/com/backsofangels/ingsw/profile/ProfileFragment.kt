9949053f4753010b4c6be530c204431c65720880
Salvatore Penitente <backsofangels@gmail.com>
2020-07-19 10:16:35 +0200

    Create profile layout

--- ingsw/src/main/java/com/backsofangels/ingsw/profile/ProfileFragment.kt
+++ ingsw/src/main/java/com/backsofangels/ingsw/profile/ProfileFragment.kt
@@ -3,7 +3,9 @@ package com.backsofangels.ingsw.profile
 import android.content.Context
 import android.util.Log
 import androidx.fragment.app.Fragment
+import com.backsofangels.ingsw.R
 import com.backsofangels.ingsw.utils.LogTags
 
-class ProfileFragment: Fragment() {
+class ProfileFragment: Fragment(R.layout.fragment_profile_layout) {
+
 }
